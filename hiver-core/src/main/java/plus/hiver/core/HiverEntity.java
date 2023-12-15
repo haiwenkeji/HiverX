da357f5138b3ff5a0ba67b8df9252577add8a341
delicacylee <delicacylee@vip.sina.com>
2023-12-15 15:51:13 +0800

    初始化项目。

---
+++ hiver-core/src/main/java/plus/hiver/core/HiverEntity.java
@@ -0,0 +1,40 @@
+/* Copyright 2014-2023 delicacylee@vip.sina.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package plus.hiver.core;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 尊重知识产权，CV 请保留版权，海文科技 https://hiver.cc 出品，不允许非法使用，后果自负
+ * </p>
+ *
+ * @author Yazhi Li
+ */
+@Getter
+@Setter
+public class HiverEntity implements Serializable {
+    /**
+     * 主键ID
+     */
+    protected String id;
+    /**
+     * 租户ID
+     */
+    protected String tenantId;
+}
